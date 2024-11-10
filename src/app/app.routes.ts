d9c5cbe1559b98cf4cf0d0971e3076dd89fdc647
Vadim <vadim_kimm@icloud.com>
2024-11-10 20:22:56 +0300

    Add lazy loading example

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './modules/home/home.component';
+import { AdminComponent } from './modules/admin/admin.component';
+import { UserComponent } from './modules/user/user.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "", redirectTo: "home", pathMatch: "full"
+  },
+  {
+    path: "home", component: HomeComponent
+  },
+  {
+    path: "user", component: UserComponent
+  },
+  {
+    path: "admin", component: AdminComponent
+    // path: "admin", loadComponent: () => import('./modules/admin/admin.component').then(m => m.AdminComponent)
+  }
+];
